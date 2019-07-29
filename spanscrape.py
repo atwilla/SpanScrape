de5ed874153f3860e0d1c5dfad7d2305e929b55d
porbhoog <alexandertwilla@gmail.com>
2019-07-29 06:44:50 -0500

    Get basic functionality up and running.

--- spanscrape.py
+++ spanscrape.py
@@ -4,10 +4,15 @@ import sys
 import urllib3
 
 word = sys.argv[1]
-url = "https://spanishdict.com/"
+url = "https://spanishdict.com/translate/"
 
 try:
 	page = requests.get(url + word)
-	soup = BS(page)
+	soup = BS(page.text, 'html.parser')
+	defList = soup.find_all("div", class_="inline--1nnau")[1:]
+
+	for defn in defList:
+		print(defn.contents[0].text)
+
 except (requests.exceptions.SSLError):
 	print("Could not connect to SpanishDict.com!")
