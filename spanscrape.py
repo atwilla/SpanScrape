406fccadd4735792489f7e419345ea3e9b946712
porbhoog <alexandertwilla@gmail.com>
2019-07-29 20:13:26 -0500

    Add code to handle result of no translations found.

--- spanscrape.py
+++ spanscrape.py
@@ -9,10 +9,20 @@ url = "https://spanishdict.com/translate/"
 try:
 	page = requests.get(url + word)
 	soup = BS(page.text, 'html.parser')
-	defList = soup.find_all("div", class_="inline--1nnau")[1:]
+	divList = soup.find_all("div", class_="inline--1nnau")[1:]
+	defList = []
 
-	for defn in defList:
-		print(defn.contents[0].text)
+	for div in divList:
+		defList.append(div.contents[0].text)
+
+	if len(defList) == 0:
+		print("No translation found.")
+
+	else:
+
+		for definition in defList:
+			print(definition)
 
 except (requests.exceptions.SSLError):
-	print("Could not connect to SpanishDict.com!")
+	print("Could not connect to %s!", (url + word))
+	print("Check internet connection, or page does not exist.")
