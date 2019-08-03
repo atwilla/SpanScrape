fb485435e2fb022428b7b5fe287465fb7bed90d4
porbhoog <alexandertwilla@gmail.com>
2019-08-03 11:05:52 -0500

    Get basic verbose functionality working.

--- spanscrape.py
+++ spanscrape.py
@@ -7,6 +7,7 @@ verbose = False
 
 if "-v" in sys.argv:
 	vIndex = sys.argv.index("-v")
+	verbose = True
 
 	if vIndex == 1:
 		word = ' '.join(sys.argv[2:])
@@ -15,26 +16,51 @@ if "-v" in sys.argv:
 else:
 	word = ' '.join(sys.argv[1:])
 
-print(word)
+# print(word)
 
 url = "https://spanishdict.com/translate/"
 
 try:
 	page = requests.get(url + word)
 	soup = BS(page.text, 'html.parser')
-	divList = soup.find_all("div", class_="inline--1nnau")[1:]
-	defList = []
 
-	for div in divList:
-		defList.append(div.contents[0].text)
+	if verbose:
+		headers = soup.find_all("div", class_="entry--3tNUi")[:-1]
+		transBlocks = []
 
-	if len(defList) == 0:
-		print("No translation found.")
+		for header in headers:
+			transBlocks += header.find_next_siblings("div")
+		
+		for block in transBlocks:
+			# Print word type.
+			print(block.contents[0].find("a").text)
+
+			# All translation text save for title found in contents[1].
+			translations = block.contents[1].contents
+
+			for trans in translations:
+				# Print word summary.
+				print("\t" + trans.contents[0].text)
+				
+				transDiv = trans.contents[1].contents[0]
+				neoDictTrans = transDiv.find("a", 
+					class_="neodictTranslation--C2TP2")
+				print("\t\t" + neoDictTrans.text)	
 
 	else:
+		divList = soup.find_all("div", class_="inline--1nnau")[1:]
+		defList = []
 
-		for definition in defList:
-			print(definition)
+		for div in divList:
+			defList.append(div.contents[0].text)
+
+		if len(defList) == 0:
+			print("No translation found.")
+
+		else:
+
+			for definition in defList:
+				print(definition)
 
 except (requests.exceptions.SSLError):
 	print("Could not connect to %s!", (url + word))
