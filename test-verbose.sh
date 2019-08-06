6353e6caf9f4b5e9960eda38908f3f874806468a
porbhoog <alexandertwilla@gmail.com>
2019-08-06 10:36:02 -0500

    Finish test files. Shorten word files for quicker testing.

--- test-verbose.sh
+++ test-verbose.sh
@@ -8,18 +8,18 @@ readarray EN_WORDS < testwords-en.txt
 
 for WORD in ${EN_WORDS[@]}; do
 	echo $WORD
-	python3 spanscrape.py $WORD
+	python3 spanscrape.py -v $WORD
 	echo 
 done
 
-echo "Testing Spanish to English...\n"
+echo "Testing Spanish to English..."
 echo
 
 readarray ES_WORDS < testwords-es.txt
 
 for WORD in ${ES_WORDS[@]}; do
 	echo $WORD
-	python3 spanscrape.py $WORD
+	python3 spanscrape.py -v $WORD
 	echo
 done
 
